be3fe09ea28069706d2af37434aadc216ae25b47
gh05twriter <168755743+gh05twriter@users.noreply.github.com>
2024-05-18 00:54:32 +0100

    Update page.tsx

--- src/app/(chat)/page.tsx
+++ src/app/(chat)/page.tsx
@@ -62,20 +62,8 @@ export default function Home() {
 
   return (
     <main className="bg-[#333233] pt-40 flex min-h-screen text-white font-sans">
-      <div className="adamtab" id="adamtab">
-        <div className="title" style="opacity:1;">Why did I build this?</div>
-          <div className="tab-head">
-            <div>Why did I build this?!</div>
-          </div>
-          <div className="tab-desc">
-            <div>
-              <p>The short answer is something something or other here, I need to copy and paste the text in at some point but it's difficult with the hover feature on figma so making stuff up</p>
-              <a href="#" class="tab-button">Read on for more detail</a>
-            </div>
-        </div>
-      </div>
 
-      <script src="query.js"></script>
+
       
       <div
         className={clsx(
